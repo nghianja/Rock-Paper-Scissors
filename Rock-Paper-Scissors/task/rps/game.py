3751133e7dbe578d292cc6469d60a26359534384
James Ng <james.ng@projapps.com>
2020-05-06 14:02:10 +0800

    Completed Stage #2

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,6 +1,14 @@
+import random
+
 # Write your code here
+
 to_win = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
-
+key_list = list(to_win.keys())
+computer = key_list[random.randint(0, 2)]
 user = input()
-
-print("Sorry, but computer chose " + to_win[user])
+if to_win[user] == computer:
+    print("Sorry, but computer chose " + computer)
+elif user == computer:
+    print("There is a draw (" + user + ")")
+else:
+    print("Well done. Computer chose " + computer + " and failed")
--- Rock-Paper-Scissors/task/tests.py
+++ Rock-Paper-Scissors/task/tests.py
@@ -7,17 +7,44 @@ CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 
 class RPSTest(StageTest):
     def generate(self) -> List[TestCase]:
-        cases = ["rock",
-                 "paper",
-                 "scissors"]
-        return [TestCase(stdin=cases[case],
-                         attach=(cases + cases)[case + 1])
-                for case in range(len(cases))]
+        self.options = ["rock", "paper", "scissors"]
+        return [TestCase(stdin=case, attach=case) for case in self.options]
 
     def check(self, reply: str, attach) -> CheckResult:
-        return CheckResult("Sorry, but computer chose {}".format(attach.strip()) == reply.strip(),
-                           "Your answer on \"{}\" was \"{}\". That's pretty wrong".format(attach, reply))
+        try:
+            if "Sorry" in reply:
+                result = -1
+                option = reply.split()[-1]
+            elif "draw" in reply:
+                result = 0
+                if '(' not in reply or ')' not in reply:
+                    return CheckResult.wrong(
+                        "There are no '(' or ')' character when there is a draw"
+                    )
+                start = reply.index('(')
+                end = reply.index(')')
+                option = reply[start + 1: end]
+            elif "Well" in reply:
+                result = 1
+                option = reply.split()[-3]
+            else:
+                raise IndexError
+            res = self.solve(result, [attach.strip(), option.strip()])
+            if res < 0:
+                raise IndexError
+        except IndexError:
+            return CheckResult.wrong("Seems like your answer (\"{}\") does not fit in given templates".format(reply))
+        return CheckResult(res, "Your answer on \"{}\" was \"{}\". That's pretty wrong".format(attach, option))
+
+    def solve(self, result, options):
+        if any(opt not in self.options for opt in options):
+            return -1
+        diff = self.options.index(options[0]) - self.options.index(options[1])
+        if not diff:
+            true_result = 0
+        else:
+            true_result = (-1) ** ((abs(diff) - (len(self.options) // 2) > 0) == (diff > 0))
+        return true_result == result
 
 
-if __name__ == '__main__':
-    RPSTest("rps.game").run_tests()
+RPSTest("rps.game").run_tests()
