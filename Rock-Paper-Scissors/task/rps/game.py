bf3d459e73ec0fc63c38b8f3a9279003657bb450
James Ng <james.ng@projapps.com>
2020-05-06 14:07:13 +0800

    Completed Stage #3

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -4,11 +4,19 @@ import random
 
 to_win = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
 key_list = list(to_win.keys())
-computer = key_list[random.randint(0, 2)]
-user = input()
-if to_win[user] == computer:
-    print("Sorry, but computer chose " + computer)
-elif user == computer:
-    print("There is a draw (" + user + ")")
-else:
-    print("Well done. Computer chose " + computer + " and failed")
+
+while True:
+    computer = key_list[random.randint(0, 2)]
+    user = input()
+
+    if user == "!exit":
+        print("Bye!")
+        break
+    if user not in key_list:
+        print("Invalid input")
+    elif to_win[user] == computer:
+        print("Sorry, but computer chose " + computer)
+    elif user == computer:
+        print("There is a draw (" + computer + ")")
+    else:
+        print("Well done. Computer chose " + computer + " and failed")
--- Rock-Paper-Scissors/task/tests.py
+++ Rock-Paper-Scissors/task/tests.py
@@ -8,35 +8,80 @@ CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 class RPSTest(StageTest):
     def generate(self) -> List[TestCase]:
         self.options = ["rock", "paper", "scissors"]
-        return [TestCase(stdin=case, attach=case) for case in self.options]
+        cases = ["rock\npaper\npaper\nscissors\nblabla\n!exit",
+                 "rock\ninvalid\n!exit",
+                 "rock\nrock\nrock\nrock\n!exit"] * 10
+        return [TestCase(stdin=case, attach=case) for case in cases]
 
     def check(self, reply: str, attach) -> CheckResult:
-        try:
-            if "Sorry" in reply:
-                result = -1
-                option = reply.split()[-1]
-            elif "draw" in reply:
-                result = 0
-                if '(' not in reply or ')' not in reply:
-                    return CheckResult.wrong(
-                        "There are no '(' or ')' character when there is a draw"
-                    )
-                start = reply.index('(')
-                end = reply.index(')')
-                option = reply[start + 1: end]
-            elif "Well" in reply:
-                result = 1
-                option = reply.split()[-3]
-            else:
-                raise IndexError
-            res = self.solve(result, [attach.strip(), option.strip()])
-            if res < 0:
-                raise IndexError
-        except IndexError:
-            return CheckResult.wrong("Seems like your answer (\"{}\") does not fit in given templates".format(reply))
-        return CheckResult(res, "Your answer on \"{}\" was \"{}\". That's pretty wrong".format(attach, option))
 
-    def solve(self, result, options):
+        reply = reply.split("\n")[:-1]
+        reply = [rep for rep in reply if len(rep.strip()) != 0]
+
+        attaches = attach.split('\n')[:-1]
+
+        if len(reply) == 0:
+            return CheckResult.wrong(
+                "Looks like you didn't output anything!"
+            )
+
+        for reply_part, attach_part in zip(reply, attaches):
+            try:
+                if "Sorry" in reply_part:
+                    result = -1
+                    option = reply_part.split()[-1]
+                elif "draw" in reply_part:
+                    result = 0
+                    if '(' not in reply_part or ')' not in reply_part:
+                        return CheckResult.wrong(
+                            "There are no '(' or ')' character when there is a draw"
+                        )
+                    start = reply_part.index('(')
+                    end = reply_part.index(')')
+                    option = reply_part[start + 1: end]
+                elif "Well" in reply_part:
+                    result = 1
+                    option = reply_part.split()[-3]
+                elif "Invalid input" in reply_part:
+                    result = 2
+                    if attach_part in self.options:
+                        return CheckResult.wrong(
+                            'Looks like you output "Invalid input" '
+                            'in the wrong place'
+                        )
+                else:
+                    raise IndexError
+
+                if attach_part not in ['!exit'] + self.options:
+                    if result == 2:
+                        res = True
+                    else:
+                        return CheckResult.wrong(
+                            "Looks like you didn't handle an invalid input correctly"
+                        )
+                else:
+                    res = self.solve(
+                        result,
+                        attach_part.strip(),
+                        option.strip()
+                    )
+
+                if res is False:
+                    return CheckResult.wrong(
+                        "You chose " + attach_part + ", "
+                        "computer chose " + option + '. '
+                        'And the answer was \"' + reply_part + '\". '
+                        'That\'s wrong reply'
+                    )
+                if res < 0:
+                    raise IndexError
+            except IndexError:
+                return CheckResult.wrong(
+                    "Seems like your answer (\"{}\") "
+                    "does not fit in given templates".format(reply_part))
+        return CheckResult.correct()
+
+    def solve(self, result, *options):
         if any(opt not in self.options for opt in options):
             return -1
         diff = self.options.index(options[0]) - self.options.index(options[1])
@@ -47,4 +92,5 @@ class RPSTest(StageTest):
         return true_result == result
 
 
-RPSTest("rps.game").run_tests()
+if __name__ == '__main__':
+    RPSTest("rps.game").run_tests()
